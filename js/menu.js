a52acd4f278160a326e00aefadb4d89aaf0b18f7
RAUL ENRIQUE ORTIZ GUTIERREZ <frontendpolitecnico2021>
2021-04-20 21:02:18 -0500

    se mejora diseño

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+            if(document.body.scrollTop > 300)
+                    $('#fixmenu').fadeIn( "slow", function() { });
+            else    
+                    $('#fixmenu').fadeOut( "slow", function() { });
+    });
+
+    $('a#srolltotop').click(function(){
+            $('html, body').animate({scrollTop:0}, 100);
+            return false;
+    });
+});
